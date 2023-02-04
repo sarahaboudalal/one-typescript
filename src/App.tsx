9e96951e067c7c686f9461e6d3a7c5c9b300fb93
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-02-04 21:13:49 +0200

    useReducer and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Counter from './components/state/Counter';
 import LoggedIn from './components/state/LoggedIn';
 import User from './components/state/User';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <LoggedIn />
       <User />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/state/Counter.tsx
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react'
+
+
+const initialState = { count: 0 }
+
+type CounterState = {
+    count: number
+}
+type CounterAction = {
+    type: string
+    payload: number
+}
+
+function reducer(state: CounterState, action: CounterAction) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + action.payload }
+        case 'decrement':
+            return { count: state.count - action.payload }
+        default:
+            return state
+    }
+}
+
+export default function Counter() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return (
+        <div>
+            <h1>Count: {state.count}</h1>
+            <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment 10</button>
+            <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement 10</button>
+        </div>
+    )
+}
