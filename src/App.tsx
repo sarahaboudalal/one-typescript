11c64e1c62235d404bc0e0848594243bd6f4d4f1
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-02-04 16:59:25 +0200

    type assertion

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <LoggedIn />
       <User />
-
     </div>
   );
 }
--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -5,22 +5,22 @@ type AuthUser = {
 }
 
 export default function User() {
-    const [user, setUser] = useState<null | AuthUser>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogIn = () => {
         setUser({
             name: 'Sarah',
             email: 'sarah@smth.com'
         })
     }
-    const handleLogOut = () => {
-        setUser(null)
-    }
+    // const handleLogOut = () => {
+    //     setUser(null)
+    // }
     return (
         <div>
             <button onClick={handleLogIn}>Login</button>
-            <button onClick={handleLogOut}>Logout</button>
-            <p>User name is {user?.name}</p>
-            <p>User email is {user?.email}</p>
+            {/* <button onClick={handleLogOut}>Logout</button> */}
+            <p>User name is {user.name}</p>
+            <p>User email is {user.email}</p>
         </div>
     )
 }
