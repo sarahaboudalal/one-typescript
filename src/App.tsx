a8bb06f85e5353c64f0bd26ebedbe355e2c3ff94
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-02-04 15:37:27 +0200

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,23 @@
 import './App.css';
+import Button from './components/Button';
 import { Greet } from './components/Greet';
 import Heading from './components/Heading';
+import Input from './components/Input';
 import Oscar from './components/Oscar';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
 import Status from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Sarah'
-  }
-  const nameList = [
-    {
-      first: 'Heba',
-      last: ' AD'
-    },
-    {
-      first: "Mohammed",
-      last: "AD"
-    },
-    {
-      first: "Taha",
-      last: "AD"
-    }
-  ]
+
+
   return (
     <div className="App">
       <Greet name='Sarah' isLoggedIn={true} />
-      <Person name={personName} />
-      <PersonList name={nameList} />
       <Status status='success' />
       <Oscar>
         <Heading>And here you are, learning Typescript!</Heading>
       </Oscar>
+      <Input handleChange={event => console.log(event.target.value)} />
+      <Button handleClick={(event, id) => console.log('i was clicked', event, id)} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -8,7 +8,7 @@ export const Greet = (props: GreetProps) => {
     const { age = 23 } = props
     return (
         <div>
-            {props.isLoggedIn ? <p>Hello {props.name}!  You're {age} years old :)</p> : <p>Hello There!</p>}
+            {props.isLoggedIn ? <h1>Hello {props.name}!  You're {age} years old :)</h1> : <h1>Hello There!</h1>}
         </div>
     )
 }
